c1b9c838d21e3f908b885991a2a9bc38f7f61e34
alpha rinzler <94365427+Yashasvi-kumar-shukla@users.noreply.github.com>
2023-07-27 18:12:18 +0530

    Update snake.py

--- snake.py
+++ snake.py
@@ -1,22 +1,4 @@
 from turtle import Turtle
-#basic method
-# class Snake:
-#     def __init__(self):
-#         self.segments=[]
-#         starting_position = [(0,0), (-20,0), (-40,0)]
-#         for position in starting_position:
-#             new_segment = Turtle("square")
-#             new_segment.color("white")
-#             new_segment.penup()
-#             new_segment.goto(position)
-#             self.segments.append(new_segment)
-#     def move(self):
-#         for seg_num in range(len(self.segments)-1,0,-1):
-#             new_x=self.segments[seg_num-1].xcor()
-#             new_y=self.segments[seg_num-1].ycor()
-#             self.segments[seg_num].goto(new_x,new_y)
-#         self.segments[0].forward(20)
-#using tuple and loop
 starting_position = [(0,0), (-20,0), (-40,0)]
 up=90
 down=270
